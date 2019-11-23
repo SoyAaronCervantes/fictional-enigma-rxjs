00e0d983f2ec6d2bea2f8abb52412ec60f3c527f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 00:37:59 -0600

    2.2 Unsubscribe con método add

--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,14 @@ const subs1 = intervalo$.subscribe(observer);
 const subs2= intervalo$.subscribe(observer);
 const subs3 = intervalo$.subscribe(observer);
 
+// Se utiliza para agregar más subscriptores
+subs1.add( subs2 ).add( subs3 );
+
 setTimeout( () => {
 
     subs1.unsubscribe();
-    subs2.unsubscribe();
-    subs3.unsubscribe();
+    // subs2.unsubscribe();
+    // subs3.unsubscribe();
 
     console.log( 'Completado timeout' );
 
