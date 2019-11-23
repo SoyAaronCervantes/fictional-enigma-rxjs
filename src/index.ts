77ebdb84c3b641d7c6ad528bb870b6ebf4e1f8fb
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 00:13:38 -0600

    1. Observables básico. Creando un observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
+import {Observable, Observer} from 'rxjs';
 
+const observer: Observer<any> = {
+    next: value => console.log('Next:', value),
+    error: err => console.error( 'Error', err ),
+    complete: () => console.info('Completado')
+};
 
-console.log('Hola Mundo!');
+// const obs$ = Observable.create()
+const obs$ = new Observable<string>( subscriber => {
 
+    subscriber.next( 'Hola' );
+    subscriber.next( 'Mundo' );
 
+    subscriber.next( 'Hola' );
+    subscriber.next( 'Mundo' );
 
+    // Forzar un error;
+    // const a  = undefined;
+    // a.nombre = 'Fernando';
 
+    subscriber.complete();
 
+    subscriber.next( 'Hola' );
+    subscriber.next( 'Mundo' );
 
+});
 
+// obs$.subscribe(
+//     res => console.log( 'next: ', res ),
+//     err => console.warn( 'error: ', err ),
+//     () => console.info( 'Complete' )
+// );
+
+obs$.subscribe( observer );
