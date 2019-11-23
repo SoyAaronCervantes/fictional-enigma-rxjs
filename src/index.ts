9e5c0d3a8517c4c60a0006a06aeebd1ebfb61303
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 00:27:09 -0600

    2.0 Multiples subscripciones, multiples instancias

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,22 @@ const observer: Observer<any> = {
     error: err => console.error( 'Error', err ),
     complete: () => console.info('Completado')
 };
+
+const intervalo$ = new Observable<number>( subscriber => {
+    // Crear un contador
+    let count: number = 0;
+
+    setInterval( () => subscriber.next( count++ ), 1000);
+});
+
+const subs1 = intervalo$.subscribe( num => console.log( 'Num: ', num) );
+const subs2= intervalo$.subscribe( num => console.log( 'Num: ', num) );
+const subs3 = intervalo$.subscribe( num => console.log( 'Num: ', num) );
+
+setTimeout( () => {
+
+    subs1.unsubscribe();
+    subs2.unsubscribe();
+    subs3.unsubscribe();
+
+    }, 3000 );
