8a6692c9540794090b01480c033cbeb721e4e47d
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 00:31:56 -0600

    2.1 Limpieza de intervalos, y como manejarlo

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,26 @@ const intervalo$ = new Observable<number>( subscriber => {
     // Crear un contador
     let count: number = 0;
 
-    setInterval( () => subscriber.next( count++ ), 1000);
+    const interval = setInterval( () => {
+        subscriber.next( count++ );
+        console.log( count );
+    }, 1000);
+    return () => {
+        clearInterval( interval );
+        console.log( 'Intervalo limpiado' );
+    }
 });
 
-const subs1 = intervalo$.subscribe( num => console.log( 'Num: ', num) );
-const subs2= intervalo$.subscribe( num => console.log( 'Num: ', num) );
-const subs3 = intervalo$.subscribe( num => console.log( 'Num: ', num) );
+const subs1 = intervalo$.subscribe();
+const subs2= intervalo$.subscribe();
+const subs3 = intervalo$.subscribe();
 
 setTimeout( () => {
 
     subs1.unsubscribe();
     subs2.unsubscribe();
     subs3.unsubscribe();
+
+    console.log( 'Completado timeout' );
 
     }, 3000 );
