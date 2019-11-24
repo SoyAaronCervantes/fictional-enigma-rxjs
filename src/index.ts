437cfa201c36409558870d4cf689886c22975e8b
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 21:18:42 -0600

    4. Creando nuestro primera funcion of

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,13 @@
-import {Observable, Observer, Subject} from 'rxjs';
+import { of } from "rxjs";
 
-const observer: Observer<any> = {
-    next: value => console.log('Next:', value),
-    error: err => console.error( 'Error', err ),
-    complete: () => console.info('Completado')
-};
+// const obs$ = of(1,2,3,4,5,6);
+const obs$ = of<number>(...[1,2,3,4,5,6],1,2,3,4,5,6,7,6,5,4);
+// const obs$ = of([1,2], { a:1,b:2 }, () => {}, true, Promise.resolve( true ));
 
-const interval$ = new Observable<number>( subscriber => {
-
-    const interval = setInterval( () => subscriber.next( Math.random() ), 1000);
-    return () => {
-        clearInterval( interval );
-        console.log( 'Intervalo destruido' );
-    };
-
-});
-/**
- * Subject
- *
- * 1. Tiene un casteo multiple. Esto quiere decir que muchas subscripciones van a estar sujetas a este mimso observable
- * 2. También es un observer
- * 3. También se puede manejar el next, error, y complete
- */
-const subject$ = new Subject();
-
-const intervalSubject = interval$.subscribe( subject$ );
-
-// const sub1 = interval$.subscribe( res => console.log('Subs 1:', res) );
-// const sub2 = interval$.subscribe( res => console.log('Subs 2:', res) );
-
-const sub1 = subject$.subscribe( observer );
-const sub2 = subject$.subscribe( observer );
-
-
-setTimeout( () => {
-
-    subject$.next( 10 );
-
-    subject$.complete();
-
-    intervalSubject.unsubscribe();
-
-}, 3500 );
+console.log('Inicio Obs');
+obs$.subscribe(
+    res => console.log( 'Next: ', res),
+    null,
+    () => console.log('Terminamos la secuencia')
+);
+console.log('Fin Obs');
