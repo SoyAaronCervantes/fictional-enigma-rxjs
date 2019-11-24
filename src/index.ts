6678e8c39f78acb868ec8710c2b28e82783f348d
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-11-23 20:30:26 -0600

    3. Empezemos con subject

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,3 @@ const observer: Observer<any> = {
     error: err => console.error( 'Error', err ),
     complete: () => console.info('Completado')
 };
-
-const intervalo$ = new Observable<number>( subscriber => {
-    // Crear un contador
-    let count: number = 0;
-
-    const interval = setInterval( () => {
-        subscriber.next( count++ );
-        console.log( count );
-    }, 1000);
-
-    setTimeout( () => subscriber.complete(), 2500 );
-    return () => {
-        clearInterval( interval );
-        console.log( 'Intervalo limpiado' );
-    }
-
-});
-
-const subs1 = intervalo$.subscribe(observer);
-const subs2= intervalo$.subscribe(observer);
-const subs3 = intervalo$.subscribe(observer);
-
-// Se utiliza para agregar más subscriptores
-subs1.add( subs2 ).add( subs3 );
-
-setTimeout( () => {
-
-    subs1.unsubscribe();
-    // subs2.unsubscribe();
-    // subs3.unsubscribe();
-
-    console.log( 'Completado timeout' );
-
-    }, 6000 );
