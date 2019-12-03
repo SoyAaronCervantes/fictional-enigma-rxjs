b3a315dccd503b7ed23bb94cd5335d99bb9325d5
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-03 14:57:04 -0600

    Cleaned index.ts

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import {asapScheduler, of, range} from 'rxjs';
-
-// const src$ = of( 1,2,3,4,5 );
-const src$ = range(1, 5, asapScheduler);
-
-console.log('Inicio');
-src$.subscribe( console.log );
-console.log('Fin');
