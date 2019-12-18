2400289d081e032744089a9421c482a57b819219
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-18 12:19:51 -0600

    Implementaciones a profunidad del timer

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ const observer = {
   complete: () => { console.log( 'completado' ) }
 };
 
+const hoyEn5 = new Date();
+hoyEn5.setSeconds( hoyEn5.getSeconds() + 5 );
+
 const interval$ = interval(1000);
-const timer$ = timer( 2000 );
+
+// const timer$ = timer( 2000 );
+// const timer$ = timer( 2000, 1000 );
+const timer$ = timer( hoyEn5 );
 
 console.log('Inicio');
 // interval$.subscribe( observer );
