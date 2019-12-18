2cf5465d65d2d61efec5bc187f5cb5ded89d6eec
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-18 12:14:10 -0600

    Primeros inicios con timer e interval

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {interval, timer} from 'rxjs';
+
+const observer = {
+  next: val => console.log( 'next', val ),
+  complete: () => { console.log( 'completado' ) }
+};
+
+const interval$ = interval(1000);
+const timer$ = timer( 2000 );
+
+console.log('Inicio');
+// interval$.subscribe( observer );
+timer$.subscribe( observer );
+console.log('Fin');
