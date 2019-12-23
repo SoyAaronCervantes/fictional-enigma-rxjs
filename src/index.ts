e4e58661a02055efc93bb9e89941d38d90966e46
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-23 02:10:47 -0600

    Operador reduce

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import {interval} from "rxjs";
+import {reduce, take, tap} from "rxjs/operators";
+
 const numbers = [1,2,3,4,5];
 
 const totalReducer = ( acumulador: number, valorActual: number ) => acumulador + valorActual;
@@ -5,3 +8,14 @@ const totalReducer = ( acumulador: number, valorActual: number ) => acumulador +
 const total = numbers.reduce( totalReducer, 0 );
 
 console.log( total );
+
+interval( 500 )
+    .pipe(
+        take( 6 ),
+        tap( console.log ),
+        reduce( totalReducer )
+    )
+    .subscribe({
+        next: val => console.log('next:', val),
+        complete: () => console.log('complete')
+    });
