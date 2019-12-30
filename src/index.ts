79abb9abb65cf70395fdfb9cf18c95d973521769
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-30 00:24:08 -0600

    Operator first, and destructuración de objetos

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import {of} from "rxjs";
-import {take, tap} from "rxjs/operators";
+import {fromEvent} from "rxjs";
+import {first, map, tap} from "rxjs/operators";
 
-const numeros$ = of(1,2,3,4,5)
-    .pipe(
-        tap( console.log ),
-    );
+const event$ = fromEvent<MouseEvent>(document, 'click');
 
-numeros$
+event$
     .pipe(
-        tap( value => console.log('%c tap', 'color: pink', value ) ),
-        take(3)
+        tap<MouseEvent>( value => console.log( '%c tap: ', 'color: pink', value ) ),
+        // first( value => value.clientY > 150 )
+
+        // map( value => ({
+        //     clientY: value.clientY,
+        //     clientX: value.clientX
+        // }) )
+
+        map( ({ clientY, clientX } ) => ({clientY, clientX})) ,
+        first( value => value.clientY > 150 )
     )
     .subscribe({
-        next: value => console.log('value: ', value),
-        complete: () => console.log('complete')
+        next: value => console.log('%c value: ', 'color: lightblue', value),
+        complete: () => console.log('completed')
     });
