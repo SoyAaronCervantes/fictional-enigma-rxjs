144c791637e1f46e7e2b6bb6975aefc8f63dd802
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-30 19:27:41 -0600

    Operator time: sampleTime

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-import {asyncScheduler, fromEvent} from "rxjs";
-import { distinctUntilChanged, pluck, tap, throttleTime } from "rxjs/operators";
+import {fromEvent} from "rxjs";
+import {map, pluck, sampleTime} from "rxjs/operators";
 
 const click$ = fromEvent<MouseEvent>(document, 'click');
 
 click$
     .pipe(
-        throttleTime(3000)
-    );
-    // .subscribe(console.log);
-
-const input = document.createElement('input');
-
-document.querySelector('body').append( input );
-
-const input$ = fromEvent<KeyboardEvent>( input, 'keyup' );
-
-input$
-    .pipe(
-        throttleTime(1000, asyncScheduler, {
-            leading: false,
-            trailing: true
-        }),
-        pluck('target', 'value'),
-        distinctUntilChanged()
+        sampleTime(2000),
+        map( ({ x, y })  => ({ x, y }) ),
     )
     .subscribe( console.log );
-
