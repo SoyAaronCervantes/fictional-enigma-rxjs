1cb422f060a1f868e9b558ea61805c1458f05472
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-30 23:34:36 -0600

    Operator getJSON

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,13 @@
-import {ajax, AjaxError} from "rxjs/ajax";
-import {catchError, map, pluck} from "rxjs/operators";
-import {of} from "rxjs";
+import {ajax} from "rxjs/ajax";
 
-const url = 'https://api.github.com/users?per_page=5';
+const url = 'https://httpbin.org/delay/1';
+// const url = 'https://api.github.com/users?per_page=5';
 
-const errorManage = ( resp: Response ) => {
-    if ( !resp.ok ) { throw new Error( resp.statusText ) }
-    return resp;
-};
+const ajax$ = ajax.getJSON( url, {
+    'content-type': 'application/json',
+    'mi-token': 'abc123'
+});
 
-const errorManageAjax = ( error: AjaxError ) => {
-  console.warn( 'Error:', error );
-  return of([]);
-};
-
-const fetchPromise = fetch( url );
-
-// fetchPromise
-//     .then( errorManage )
-//     .then( value => value.json() )
-//     .then( value => console.log('Data: ', value ) )
-//     .catch( err => console.warn('Error: ', err ) );
-
-const ajax$ = ajax( url );
-
-ajax$
-    .pipe(
-        pluck('response'),
-        catchError( errorManageAjax )
-    )
-    .subscribe( console.log );
+ajax$.subscribe({
+    next: value => console.log('%c Data:', 'color: yellow', value)
+});
