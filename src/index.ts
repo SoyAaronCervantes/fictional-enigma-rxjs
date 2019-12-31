01a86c0052323d2161d9ebbbc437c60e93caab63
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-30 18:28:02 -0600

    Operator distinctUntilChange

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import {from, of} from "rxjs";
-import {distinct} from "rxjs/operators";
+import {distinct, distinctUntilChanged} from "rxjs/operators";
 
 const numbers$ = of(1,1,1,3,3,2,2,4,5,3,1);
 
 numbers$
     .pipe(
-        distinct()
+        distinctUntilChanged()
     )
     .subscribe( console.log );
 
@@ -15,16 +15,17 @@ interface Personaje {
 
 const personajes: Personaje[] = [
     { nombre: 'Megaman' },
+    { nombre: 'Megaman' },
     { nombre: 'X' },
     { nombre: 'Zero' },
     { nombre: 'Dr. Willy' },
     { nombre: 'X' },
-    { nombre: 'Megaman' },
+    { nombre: 'X' },
     { nombre: 'Zero' },
 ];
 
 from( personajes )
     .pipe(
-        distinct( value => value.nombre )
+        distinctUntilChanged( (x, y) => x.nombre === y.nombre )
     )
     .subscribe( console.log );
