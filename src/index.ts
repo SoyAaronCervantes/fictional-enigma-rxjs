8a8144df42a8f5023cbf0e7f18c68396b2465ff0
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-31 21:26:56 -0600

    Added imports

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import {fromEvent, Observable} from "rxjs";
+import {debounceTime, map, mergeAll, pluck} from "rxjs/operators";
+import {ajax} from "rxjs/ajax";
+import {GithubUser} from "./interfaces/github-user.interface";
+import {GithubUsers} from "./interfaces/github-users.interface";
+
+const body = document.querySelector('body');
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+
+body.append( textInput, orderList );
+
+// Helpers
+
+const showUser = ( users: GithubUser[] ) => {
+
+    console.log( users );
+
+    orderList.innerHTML = '';
+
+    for (let user of users) {
+
+        const listItem = document.createElement('li');
+
+        const img = document.createElement('img');
+
+        const anchor = document.createElement('a');
+
+        img.src = user.avatar_url;
+
+        anchor.href = user.html_url;
+        anchor.text = 'Ir a la página';
+        anchor.target = '_blank';
+
+        listItem.append( img );
+
+        listItem.append( document.createElement('br') );
+
+        listItem.append( user.login + ' ' );
+
+        listItem.append( document.createElement('br') );
+
+        listItem.append( anchor );
+
+        listItem.append( document.createElement('br') );
+
+        orderList.append( listItem );
+
+    }
+
+};
+
+// Stream
+
+const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup' );
+
+input$
+    .pipe(
+        debounceTime(1000),
+        pluck<KeyboardEvent, string>('target', 'value'),
+        map<string, Observable<GithubUsers>>( value => ajax.getJSON(`https://api.github.com/search/users?q=${ value }`) ),
+        mergeAll(),
+        pluck('items'),
+    )
+    .subscribe( showUser );
