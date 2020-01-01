2723ad0f5e9dd89c254261b4fde62bac60ab27d8
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-31 21:24:29 -0600

    Operator transform: MergeMap

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,34 @@
-import {fromEvent, Observable} from "rxjs";
-import {debounceTime, map, mergeAll, pluck} from "rxjs/operators";
-import {ajax} from "rxjs/ajax";
-import {GithubUsers} from "./interfaces/github-users.interface";
-import {GithubUser} from "./interfaces/github-user.interface";
+import {fromEvent, interval, of} from "rxjs";
+import {map, mergeMap, take, takeUntil} from "rxjs/operators";
 
-const body = document.querySelector('body');
-const textInput = document.createElement('input');
-const orderList = document.createElement('ol');
+const letras$ = of('a','b','c');
 
-body.append( textInput, orderList );
-
-// Helpers
-
-const showUser = ( users: GithubUser[] ) => {
-
-  console.log( users );
-
-  orderList.innerHTML = '';
-
-    for (let user of users) {
-
-        const listItem = document.createElement('li');
-
-        const img = document.createElement('img');
-
-        const anchor = document.createElement('a');
-
-        img.src = user.avatar_url;
-
-        anchor.href = user.html_url;
-        anchor.text = 'Ir a la página';
-        anchor.target = '_blank';
-
-        listItem.append( img );
-
-        listItem.append( document.createElement('br') );
-
-        listItem.append( user.login + ' ' );
-
-        listItem.append( document.createElement('br') );
-
-        listItem.append( anchor );
-
-        listItem.append( document.createElement('br') );
-
-        orderList.append( listItem );
-
-    }
-
-};
-
-// Stream
-
-const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup' );
-
-input$
+letras$
     .pipe(
-        debounceTime(1000),
-        pluck<KeyboardEvent, string>('target', 'value'),
-        map<string, Observable<GithubUsers>>( value => ajax.getJSON(`https://api.github.com/search/users?q=${ value }`) ),
-        mergeAll(),
-        pluck('items'),
+        mergeMap( letter =>
+            interval(1000)
+                .pipe(
+                    map( i => letter + i ),
+                    take(3)
+                )
+        )
+    );
+// .subscribe({
+//     next: value => console.log('%c Valor', 'color: pink', value),
+//     complete: () =>  console.log('%c Complete', 'color: lightgreen')
+// });
+
+const mouseDown$ = fromEvent<MouseEvent>( document, 'mousedown' );
+const mouseUp$ = fromEvent<MouseEvent>( document, 'mouseup' );
+const interval$ = interval();
+
+mouseDown$
+    .pipe(
+        mergeMap( () => interval$
+            .pipe(
+                takeUntil( mouseUp$ )
+            )
+        )
     )
-    .subscribe( showUser );
+    .subscribe( console.log );
+
