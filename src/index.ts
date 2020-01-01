f66983ee2694ab11aa22e66b1f8f443f6ac9b3ed
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-01-01 01:08:37 -0600

    Operator combination: forkJoin lab

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import {forkJoin, interval, of} from "rxjs";
-import {delay, take} from "rxjs/operators";
+import {forkJoin} from "rxjs";
+import {ajax} from "rxjs/ajax";
 
-const numeros$ = of(1,2,3,4);
-const interval$ = interval(1000).pipe( take(3) );
-const letras$ = of('a','b','c','d').pipe( delay(3500) );
-
-//forkJoin( numeros$, interval$, letras$ ).subscribe( console.log );
-
-// forkJoin( numeros$, interval$, letras$ ).subscribe( value => {
-//     console.log('numeros:', value[0] );
-//     console.log('intervalo:', value[1] );
-//     console.log('letras:', value[2] );
-// });
-
-// forkJoin({ numeros$, interval$, letras$ }).subscribe( console.log );
+const github_API_URL = 'https://api.github.com/users';
+const github_USER = 'soyaaroncervantes';
 
 forkJoin({
-    num: numeros$,
-    int: interval$,
-    let: letras$
-}).subscribe( value => console.log( value.int ) );
+    usuario: ajax.getJSON(`${ github_API_URL }/${github_USER}`),
+    repos: ajax.getJSON(`${ github_API_URL }/${github_USER}/repos`),
+    gists: ajax.getJSON(`${ github_API_URL }/${github_USER}/gists`),
+}).subscribe( console.log );
