d73360016f0a381bb03478a6ce77f00a7586e78d
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-01-01 01:02:00 -0600

    Operator combination: forkJoin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import {concat, interval, of} from "rxjs";
-import {take} from "rxjs/operators";
+import {forkJoin, interval, of} from "rxjs";
+import {delay, take} from "rxjs/operators";
 
-const interval$ = interval(1000);
+const numeros$ = of(1,2,3,4);
+const interval$ = interval(1000).pipe( take(3) );
+const letras$ = of('a','b','c','d').pipe( delay(3500) );
 
-concat(
-    interval$.pipe( take(3) ),
-    interval$.pipe( take(2) ),
-    of(1)
-).subscribe( console.log );
+//forkJoin( numeros$, interval$, letras$ ).subscribe( console.log );
+
+// forkJoin( numeros$, interval$, letras$ ).subscribe( value => {
+//     console.log('numeros:', value[0] );
+//     console.log('intervalo:', value[1] );
+//     console.log('letras:', value[2] );
+// });
+
+// forkJoin({ numeros$, interval$, letras$ }).subscribe( console.log );
+
+forkJoin({
+    num: numeros$,
+    int: interval$,
+    let: letras$
+}).subscribe( value => console.log( value.int ) );
