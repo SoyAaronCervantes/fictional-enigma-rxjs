ade634db7a0a3eea441b4e3d4ae02692cc08d451
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-31 23:16:33 -0600

    Operator transform: ejercicio de swithMap, mergeMap, exhaustMap. En un caso de la vida real

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,60 @@
-import {fromEvent, interval} from "rxjs";
-import {concatMap, exhaustMap, take} from "rxjs/operators";
+// Form [ HTML ]
 
-/**
- * exhaustMap: Mantiene una subscripción activa, y si una subscripción es emitida mientras tiene una
- * subscripción activa es ignorada. Pero si no existe una subscripción activa y es emitida un nuevo valor,
- * este se tomará como subscripción activa.
- */
+import {fromEvent, of} from "rxjs";
+import {catchError, exhaustMap, map, mergeMap, pluck, switchMap, tap} from "rxjs/operators";
+import {ajax, AjaxResponse} from "rxjs/ajax";
 
-const interval$ = interval(500).pipe( take(3) );
-const click$ = fromEvent<MouseEvent>( document, 'click' );
+const form = document.createElement('form');
+const inputEmail = document.createElement('input');
+const inputPassword = document.createElement('input');
+const submitBtn = document.createElement('button');
+const body = document.querySelector('body');
 
-click$
+// Config
+
+inputEmail.type = 'email';
+inputEmail.placeholder = 'Email';
+inputEmail.value = 'eve.holt@reqres.in';
+
+inputPassword.type = 'password';
+inputPassword.placeholder = 'Password';
+inputPassword.value = 'cityslicka';
+
+submitBtn.innerHTML = 'Submit';
+submitBtn.type = 'submit';
+
+form.append( inputEmail, inputPassword, submitBtn );
+body.append( form );
+
+interface userLogin {
+    email: string;
+    password: string;
+}
+
+// functions
+const url = 'https://reqres.in/api/login?delay=1';
+const httpLoginRequest =
+    ( userPass: userLogin ) =>
+        ajax.post( url, userPass )
+            .pipe(
+                pluck('response', 'token'),
+                catchError( err => of('xxx') )
+            );
+
+// streams
+const submitForm$ = fromEvent<Event>( form, 'submit' )
     .pipe(
-        exhaustMap( () => interval$ )
+        tap( x => x.preventDefault() ),
+        map<Event, userLogin>( value => ({
+                email: value.target[0].value,
+                password: value.target[1].value
+            })
+        ),
+        // mergeMap( httpLoginRequest ),
+        switchMap( httpLoginRequest ),
+        // exhaustMap( httpLoginRequest ),
     )
-    .subscribe( console.log );
+    .subscribe({
+        next: value => console.log('%c [TOKEN] value:', 'color: pink', value),
+        error: err =>  console.warn('%c [TOKEN] error:', 'color: pink', err),
+    });
