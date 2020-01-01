59f4381ea772981b47d2a7904b05b21e10a470d4
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-31 22:35:30 -0600

    Operator transform: exhaustMap

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import {fromEvent, interval} from "rxjs";
-import {concatMap, switchMap, take} from "rxjs/operators";
+import {concatMap, exhaustMap, take} from "rxjs/operators";
+
+/**
+ * exhaustMap: Mantiene una subscripción activa, y si una subscripción es emitida mientras tiene una
+ * subscripción activa es ignorada. Pero si no existe una subscripción activa y es emitida un nuevo valor,
+ * este se tomará como subscripción activa.
+ */
 
 const interval$ = interval(500).pipe( take(3) );
 const click$ = fromEvent<MouseEvent>( document, 'click' );
 
 click$
     .pipe(
-        concatMap( () => interval$ )
+        exhaustMap( () => interval$ )
     )
     .subscribe( console.log );
