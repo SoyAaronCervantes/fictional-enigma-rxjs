1ae629a1edd4af31b861488a3f88d80977043e66
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-31 20:27:55 -0600

    Operator GET, POST, DELETE, PUT and dynamic request

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import {ajax} from "rxjs/ajax";
 
 const url = 'https://httpbin.org/delay/1';
-// const url = 'https://api.github.com/users?per_page=5';
 
-const ajax$ = ajax.getJSON( url, {
-    'content-type': 'application/json',
+ajax.post( url, {
+    id: 1,
+    name: 'Aarón'
+}, {
     'mi-token': 'abc123'
-});
+})
+    .subscribe( value => console.log('%c [POST] Value:', 'color: pink', value) );
 
-ajax$.subscribe({
-    next: value => console.log('%c Data:', 'color: yellow', value)
-});
+ajax.put( url, {
+    id: 1,
+    name: 'Aarón'
+}, {
+    'mi-token': 'abc123'
+})
+    .subscribe( value => console.log('%c [PUT] Value:', 'color: lightyellow', value) );
+
+ajax.delete( url, { 'mi-token': 'abc123' } )
+    .subscribe( value => console.log('%c [DELETE] Value:', 'color: lightblue', value) );
+
+
+ajax({
+   url,
+   method: 'delete',
+    headers: {
+       'mi-token': 'abc123'
+    },
+    body: {
+       id: 2,
+        name: 'Aarón'
+    }
+})
+    .subscribe(  value => console.log('%c [DYNAMIC REQUEST] Value:', 'color: lightgreen', value)  );
